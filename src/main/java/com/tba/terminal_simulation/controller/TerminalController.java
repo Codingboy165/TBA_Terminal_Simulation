9fe6ce4501194d5207ee154d7f5871ddd523a064
Jonathán <solyomjonathan@gmail.com>
2023-03-16 13:38:11 +0200

    First Commit

---
+++ src/main/java/com/tba/terminal_simulation/controller/TerminalController.java
@@ -0,0 +1,50 @@
+package com.tba.terminal_simulation.controller;
+
+import com.tba.terminal_simulation.model.Gate;
+import com.tba.terminal_simulation.model.Truck;
+import com.tba.terminal_simulation.model.TruckType;
+import com.tba.terminal_simulation.service.Response;
+import com.tba.terminal_simulation.service.TruckService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TerminalController {
+
+    private final TruckService truckService;
+
+    public TerminalController(TruckService truckService){
+        this.truckService = truckService;
+    }
+
+    @PostMapping("api/truck/send")
+    public Truck createATruck(@RequestBody Truck truck){
+        return truckService.addTruckToTheInBoundLane(truck);
+    }
+
+    @PostMapping("api/trucks/send")
+    public ResponseEntity<Response> createATruck(@RequestBody List<Truck> truck){
+        return truckService.addTrucksToTheInBoundLane(truck);
+    }
+
+    @PostMapping("api/gate/create")
+    public ResponseEntity<Response> createAGate(@RequestBody Gate gate){
+        return truckService.creatingAGate(gate.getInBoundLanes(), gate.getOutBoundLanes(), gate.getHandlingLocations());
+    }
+
+    @GetMapping("api/trucks/gate")
+    public Map<TruckType,Integer> getAllTruckPerReceiveAnDeliverAtTheGate(){
+        return truckService.getAllTheTrucksFromTheQueue();
+    }
+
+    @PostMapping("api/simulation/restart")
+    public ResponseEntity<Response> restartTheSimulation(){
+        return truckService.restartTheSimulation();
+    }
+}
